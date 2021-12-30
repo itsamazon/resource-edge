f55d28ada245c52311e99fccdf26b62b1710fec4
chidera <chidera.diandra@gmail.com>
2021-12-30 14:49:08 +0100

    dropdown update

--- src/Components/dashboard/index.js
+++ src/Components/dashboard/index.js
@@ -4,10 +4,34 @@ import menu from '../../images/menu-outline.png'
 import res from '../../images/res.png'
 import tm from '../../images/TM Dashboard.svg'
 import profile from '../../images/picon.svg'
-import Dropdown from './dropdown';
-import { useNavigate } from 'react-router-dom';
+import talent from '../../images/Talent Manager.svg'
+import employ from '../../images/Employee.svg'
+import check from '../../images/check.svg'
 
 const Dashboard = () => {
+  
+    const tmfunc = () => {
+        const flexcontainer= document.getElementById('roles')
+        let drp = document.getElementById("check11");
+        const emtick = document.getElementById('check22')
+      
+          drp.style.display = "block";
+          emtick.style.display = "none"
+          flexcontainer.style.flexDirection='column'
+    }
+
+    const emfunc = () => {
+        const flexcontainer= document.getElementById('roles')
+        let drp = document.getElementById("check22");
+        const tmtick = document.getElementById('check11')
+      
+          drp.style.display = "block";
+          tmtick.style.display = "none"
+          flexcontainer.style.flexDirection='column-reverse'
+    }
+
+
+
     const checked = () => {
         // Get the checkbox
         let checkBox = document.getElementById("menucheck");
@@ -35,9 +59,34 @@ const Dashboard = () => {
                 <img id='profile' src={profile}/>
                 </div>
             </nav>
+
+            
             <div className='dropdown' id ='drpcnt'>
-            <Dropdown />
-            </div>
+                <div className='dropdown-content'>
+                    <h1>OSITADINMA NWANGWU</h1>
+                    <p>Profile</p>
+                    <sm id='sm'>Use Resource Edge as </sm>
+                    <div className='roles' id='roles'>
+                        <div className='role' id='tmrole'>
+                            <img src={talent}/>
+                            <label for='check1'>Talent Manager</label>
+                            <input type='checkbox' id='check1' onClick={tmfunc}/>
+                            <img id='check11' src={check} />
+                        </div>
+                        <div className='role' id='emrole'>
+                            <img src={employ}/>
+                            <label for='check2'>Employee</label>
+                            <input type='checkbox' id='check2' onClick={emfunc}/>
+                            <img id='check22' src={check} />
+                        </div>
+                    </div>
+                </div>
+                <div className='logout'>
+                    <li><a>Logout</a></li>
+                </div>
+        </div>
+
+
             <div className='dash_content'>
                     <h1>Hello, Ositadinma Nwangwu</h1>
                     <p>Welcome and good to have you back.</p>
